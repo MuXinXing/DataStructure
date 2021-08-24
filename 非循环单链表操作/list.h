ba7bf21e452d29b774f8931b94c03f311e69fe92
Muxin <2522334555@qq.com>
2021-08-24 16:25:50 +0800

    链表

---
+++ 非循环单链表操作/list.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef LIST_H
+#define LIST_H
+typedef struct Node {
+	int data;//数据区
+	struct Node* next;//指针域
+}*PointNode,Node;
+
+PointNode create(int len);
+void delete(PointNode pointHead, int position);
+void insert(PointNode pointHead, int position, int value);
+void showList(PointNode pointHead);
+void isEmpty(PointNode pointHead);
+int length(PointNode pointHode);
+void sort(PointNode pointHead);
+#endif // !LIST_H
+//除需要操作的链表的返回值为PointNode，其他操作函数返回值随意，方便操作即可
